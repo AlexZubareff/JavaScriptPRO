3d831b12fef73aa64c1881dfcbe97051f2e443b9
AlexZubareff <azv99@yandex.ru>
2021-05-13 00:01:41 +0300

    Стилизовал. С запятыми не разобрался.

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 `use strict`
 const goods = [
-  { title: 'Shirt', price: 150 },
-  { title: 'Socks', price: 50 },
-  { title: 'Jacket', price: 350 },
-  { title: 'Shoes', price: 250 },
+  { title: 'Shirt', price: 150, },
+  { title: 'Socks', price: 50, },
+  { title: 'Jacket', price: 350, },
+  { title: 'Shoes', price: 250,},
+  { title: 'Shirt', price: 150, },
+  { title: 'Socks', price: 50, },
+  { title: 'Jacket', price: 350, },
+  { title: 'Shoes', price: 250,},
 ];
 
-const renderGoodsItem = (title, price) => {
-  return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
+const renderGoodsItem = (title = `Пусто`, price = `Пусто`) => {
+  return `<div class="goods-item"><img class = "image"><h3>${title}</h3><p>${price}</p></div>`;
 };
 
 const renderGoodsList = (list) => {
