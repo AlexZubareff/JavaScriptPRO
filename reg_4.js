dd31734ea34009e92de339b2879a1feb04309f19
AlexZubareff <azv99@yandex.ru>
2021-05-30 11:29:40 +0300

    Попарвил reg_4.js

--- reg_4.js
+++ reg_4.js
@@ -8,7 +8,7 @@ console.log(str.replace(regexp, '"'))
 // Заменяет все одинарные ковычки на двойные только в начале о конце слова.
 
 const str = `Lorem ipsum dolor sit amet consectetur, adipisicing elit. 'Deserunt' maiores exercitationem quo. Suscipit corpori's repellendus nesciunt natus 'minima', Iste, perferendis dolores neque minima possimus quisquam soluta sit iusto quia minus?`;
-const regexp = /(\s\'|\'\W)/g
+const regexp = /(\B'|'\B)/g
 // str.match(regexp);
 // console.log(str.match(regexp))
 str.replace(regexp, '"'); 
