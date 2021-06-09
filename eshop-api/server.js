c8143a704ff3f727964a3d4612b7c1b37433403d
AlexZubareff <azv99@yandex.ru>
2021-06-09 12:50:14 +0300

    решил вопрос с отображением списка в браузере. Проблема была в Cors

--- eshop-api/server.js
+++ eshop-api/server.js
@@ -5,7 +5,13 @@ const fs = require('fs');
 app.listen(3000, () => {
     console.log(('Server is running on 3000 port!'))
 });
-
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 app.get('/catalogData', (req, res) =>{
     fs.readFile('./database/catalog.json', 'utf-8', (err, data) => {
         // console.log(data);
